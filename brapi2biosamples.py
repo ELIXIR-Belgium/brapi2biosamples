73fde392e9dd48fe2a0402fb15328fdfb34b48e6
bedroesb <bedro@psb.vib-ugent.be>
2021-02-22 16:57:32 +0100

    submition and validation

--- brapi2biosamples.py
+++ brapi2biosamples.py
@@ -1,4 +1,5 @@
 import json
+import os
 import requests
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
@@ -7,6 +8,21 @@ import click
 import yaml
 from datetime import datetime
 
+ALL_ENDPOINTS = {
+    "dev": {
+        "token": "https://explore.api.aai.ebi.ac.uk/auth",
+        "validate": "https://wwwdev.ebi.ac.uk/biosamples/samples/validate",
+        "submit": "https://wwwdev.ebi.ac.uk/biosamples/samples",
+        "sample": "https://wwwdev.ebi.ac.uk/biosamples/samples/"
+    },
+    "stable": {
+        "token": "https://api.aai.ebi.ac.uk/auth",
+        "validate": "https://www.ebi.ac.uk/biosamples/samples/validate",
+        "submit": "https://www.ebi.ac.uk/biosamples/samples",
+        "sample": "https://www.ebi.ac.uk/biosamples/samples/"
+    }
+}
+
 
 def url_path_join(*args):
     """Join path(s) in URL using slashes"""
@@ -14,30 +30,51 @@ def url_path_join(*args):
 
 
 def characteristic(text, ontology=None):
-    """Making a ENA Biosamples characteristic"""
+    """
+    Making a ENA Biosamples characteristic
+    """
     if ontology:
         return [{"text": text, "ontologyTerms": [ontology]}]
     else:
         return [{"text": text}]
 
-def fetch_token(username, password, dev):
-    if dev:
-        endpoint = 'https://explore.api.aai.ebi.ac.uk/auth'
-    else:
-        endpoint = 'https://api.aai.ebi.ac.uk/auth'
-    
+
+def fetch_token(endpoint, username, password):
+    """
+    Fetch token with username and password as parameters
+    """
+    print('  GET ' + endpoint)
     token = requests.get(endpoint, auth=(username, password))
     if token.status_code != 200:
-        print("problem with request: " + str(token))
+        print(f"Problem with request: {str(token)}")
         raise RuntimeError("Non-200 status code")
     return(token.text)
+
+
+def fetch_POST(endpoint, token, data):
+    """
+    Fetch single BrAPI object by path
+    """
+    print('  POST ' + endpoint)
+    headers = {}
+    headers['Accept'] = "application/hal+json"
+    headers['Content-Type'] = "application/json;charset=UTF-8"
+    headers['Authorization'] = f"Bearer {token}"
+    r = requests.post(endpoint, data=data, headers=headers)
+    if r.status_code == 201:
+        return r.json()
+    elif r.status_code != 200:
+        print(f"Problem with request: {str(r)}")
+        raise RuntimeError("Non-200 status code")
+    return True
+
 
 def fetch_object(endpoint, path):
     """
     Fetch single BrAPI object by path
     """
     url = url_path_join(endpoint, path)
-    print('GET ' + url)
+    print('  GET ' + url)
     session = requests.Session()
     retry = Retry(connect=3, backoff_factor=15)
     adapter = HTTPAdapter(max_retries=retry)
@@ -49,41 +86,39 @@ def fetch_object(endpoint, path):
         time.sleep(5)
         r = requests.get(url)
     elif r.status_code != 200:
-        print("problem with request: " + str(r))
+        print(f"Problem with request: {str(r)}")
         raise RuntimeError("Non-200 status code")
     return r.json()["result"]
 
 
-def fetch_objects(endpoint, path, params: dict = None):
+def fetch_objects(endpoint, path):
     """
     Fetch BrAPI objects with pagination
     """
     page = 0
     pagesize = 1000
     maxcount = 0
-    # set a default dict for parameters
-    params = params or {}
     url = url_path_join(endpoint, path)
     session = requests.Session()
     retry = Retry(connect=3, backoff_factor=15)
     adapter = HTTPAdapter(max_retries=retry)
     session.mount('http://', adapter)
     session.mount('https://', adapter)
+    params = {}
     output = []
     while maxcount == 0 or page < maxcount:
+
         params['page'] = page
         params['pageSize'] = pagesize
-        print('retrieving page ' + str(page) + ' of ' +
-              str(maxcount) + ' from ' + str(url))
-        print("paging params:" + str(params))
-        print("GET " + url)
+        print(f"  Retrieving page {page} of {str(maxcount)} from {str(url)}")
+        print("  GET " + url)
         r = session.get(url, params=params)
         if r.status_code == 504 and pagesize != 100:
             pagesize = 100
             print("504 Gateway Timeout Error, testing with pagesize = 100")
             continue
         elif r.status_code != 200:
-            print("problem with request: " + str(r))
+            print("Problem with request: " + str(r))
             raise RuntimeError("Non-200 status code")
         maxcount = int(r.json()['metadata']['pagination']['totalPages'])
 
@@ -94,6 +129,7 @@ def fetch_objects(endpoint, path, params: dict = None):
 
         return output
 
+
 @click.command(context_settings={'help_option_names': ['-h', '--help']})
 @click.version_option("0.1.0", "-v", "--version", prog_name="brapi2biosamples", help="Print version number")
 @click.option("--trialdbid", "-t", help="The identifier of a trial", required=True)
@@ -103,35 +139,45 @@ def fetch_objects(endpoint, path, params: dict = None):
 @click.option("--submit", "-s", help="When this flag is given, the samples will be submitted to BioSamples instead of being exported as JSON", is_flag=True)
 @click.option("--dev", help="When this flag is given, the samples will be submitted to the dev instance of BioSamples", is_flag=True)
 @click.option("--secret", help="Path to a secret.yml file to deliver the BioSample credentials", type=click.Path(exists=True))
-@click.option("--output", help="Path to a directory where the JSON files are written to.", type=click.Path(exists=True))
-
+@click.option("--output", help="Path to a directory where the JSON files are written to.", type=click.Path(exists=True), default=".")
 def main(trialdbid, endpoint, date, domain, submit, dev, secret, output):
     """ Submits samples to BioSamples using the Breeding API """
 
     if submit:
+        if dev:
+            print("--- This is a test submission ---")
+            ENDPOINTS = ALL_ENDPOINTS['dev']
+        else:
+            ENDPOINTS = ALL_ENDPOINTS['stable']
+
+        print("--- Requesting TOKEN ---")
         click.format_filename(secret)
         secret_file = open(secret, "r")
         credentials = yaml.load(secret_file, Loader=yaml.FullLoader)
 
         password = credentials['password'].strip()
         username = credentials['username'].strip()
-        token = fetch_token(username, password, dev)
+        token = fetch_token(ENDPOINTS['token'], username, password)
+        print("  Authentication is successful")
 
-
+    print("--- Fetching germplasm data ---")
     # Fetch studies from trial
     trial = fetch_object(endpoint, f'/trials/{trialdbid}')
     added_germplasm = []
     # Loop over studies
     for study in trial['studies']:
-        print(f"Getting germplasms from {study['studyDbId']}")
-        # Get germplasmId's from study 
-        allgermplasms = fetch_objects(endpoint, f"/studies/{study['studyDbId']}/germplasm")
+        print(f"  - Getting germplasms from {study['studyDbId']}")
+        # Get germplasmId's from study
+        allgermplasms = fetch_objects(
+            endpoint, f"/studies/{study['studyDbId']}/germplasm")
         for germplasm in allgermplasms:
             if germplasm['germplasmDbId'] not in added_germplasm:
+                print(
+                    f"  - Generating BioSamples JSON-LD for {germplasm['germplasmDbId']}")
                 added_germplasm.append(germplasm['germplasmDbId'])
                 # Get extra Germplasm information (Needed for PIPPA)
                 germplasminfo = fetch_object(endpoint,
-                    f"/germplasm/{germplasm['germplasmDbId']}")
+                                             f"/germplasm/{germplasm['germplasmDbId']}")
                 germjson = {"name": germplasminfo['germplasmDbId'],
                             "domain": domain, "release": date, "characteristics": {}}
                 germjson['characteristics']['project name'] = characteristic(
@@ -148,9 +194,27 @@ def main(trialdbid, endpoint, date, domain, submit, dev, secret, output):
                     germplasminfo['accessionNumber'])
                 germjson['characteristics']['material source ID'] = characteristic(
                     germplasminfo['germplasmName'])
-                if not submit:
-                    with open('o_' + germplasm['germplasmDbId'] + '.json', 'w') as outfile:
+
+                if submit:
+                    print(
+                        f"  - Validating the JSON-LD schema of f{germplasm['germplasmDbId']}")
+                    validate = fetch_POST(
+                        ENDPOINTS['validate'], token, json.dumps(germjson))
+                    if validate:
+                        print("  Validation was successful")
+                    print(
+                        f"  - Submitting the JSON-LD schema of f{germplasm['germplasmDbId']}")
+                    submit = fetch_POST(
+                        ENDPOINTS['submit'], token, json.dumps(germjson))
+                    print(
+                        f"  Sample was successfully submitted as:\n    Name: {submit['name']}\n    Accession: {submit['accession']}\n    URL: {ENDPOINTS['sample'] + submit['accession']}")
+                else:
+                    filepath = os.path.join(
+                        output, f"o_{germplasm['germplasmDbId']}.json")
+                    with open(filepath, 'w') as outfile:
                         outfile.write(json.dumps(germjson, indent=4))
+                    print(
+                        f"  BioSample JSON-LD from {study} dumped as {filepath}")
 
 
 if __name__ == "__main__":
